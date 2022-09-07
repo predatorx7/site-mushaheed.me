cded3ac3a49ed7b8fa87226f06c980698c3786a3
predatorx7 <smushaheed@outlook.com>
2022-09-08 04:53:19 +0530

    Add updated footer

--- main.tsx
+++ main.tsx
@@ -3,6 +3,7 @@
 import blog, { ga, h, redirects } from "blog";
 import { unocss_opts } from "./unocss.ts";
 import { links } from "./links.ts";
+import { Footer } from "./footer.tsx";
 
 blog({
   title: "Mushaheed Syed",
@@ -16,7 +17,7 @@ blog({
   unocss: unocss_opts,
   // header: <header>Your custom header</header>,
   // section: <section>Your custom section</section>,
-  // footer: <footer>© 2022 Mushaheed Syed</footer>,
+  footer: <Footer author="Mushaheed Syed" />,
   // middlewares: [
 
   // If you want to set up Google Analytics, paste your GA key here.
